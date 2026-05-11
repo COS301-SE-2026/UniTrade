e0fa33635526d90ba0b23f5602ffdf2fb55c86f2
Tafadzwa2608 <tafadzwamusiiwa08@gmail.com>
2026-05-12 00:26:06 +0200

    Added all the neccessary commits needed fo the sidebar

---
+++ src/web/src/components/layout/Sidebar.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom'
+import { clsx } from 'clsx'
+import {
+  IconLayoutDashboard,
+  IconSwitchHorizontal,
+  IconShoppingBag,
+  IconHeart,
+  IconBookmark,
+  IconMessage,
+  IconUser,
+  IconSettings,
+  IconShieldCheck,
+  IconListCheck,
+  IconFlag,
+  IconUsers,
+  IconChartBar,
+  IconPlus,
+  IconPackage,
+  IconChevronLeft,
+  IconChevronRight,
+} from '@tabler/icons-react'
+import { useAuthStore } from '../../store/useAuthStore'
+import { useState } from 'react'
+
