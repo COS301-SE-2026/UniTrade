8f24b2bc78c4f517469eebbe1c8dc6e8d5c025d9
diditlaka <diditlaka51@gmail.com>
2026-05-12 18:50:49 +0200

    added the section for the picture - just a blue colour for now , we will decide on a picture as a team

--- src/web/src/pages/auth/HomePage.tsx
+++ src/web/src/pages/auth/HomePage.tsx
@@ -29,10 +29,25 @@ function Navbar(){
     )
 }
 
+function ImageSection(){
+    return(
+        <div className = "relative w-full h-[500px] bg-[#003366] flex items-center" >
+            <div className = "absolute inset-0 bg-black/40" />
+
+            <div className = "relative z-10 px-12">
+                <h2 className = "text-white text-5xl font-extrabold uppercase leading-tight max-w-lg">
+                    University Materials Made Accessible
+                </h2>
+            </div>
+        </div>
+    )
+}
+
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Navbar />
+      <ImageSection />
       <p className="p-8 text-gray-400">One step at a time .</p>
     </div>
   )
