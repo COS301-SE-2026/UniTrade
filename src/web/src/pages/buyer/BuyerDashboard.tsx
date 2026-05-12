f97c552dd3a827658117cb5e5bcc8feee12ba28e
diditlaka <diditlaka51@gmail.com>
2026-05-12 23:34:55 +0200

    added the card section for the statitics

--- src/web/src/pages/buyer/BuyerDashboard.tsx
+++ src/web/src/pages/buyer/BuyerDashboard.tsx
@@ -1,3 +1,45 @@
+import {useNavigate} from 'react-router-dom'
+import {useAuthStore} from '../../store/useAuthStore'
+import{
+  IconShoppingBag,
+  IconCurrencyDollar,
+  IconClock,
+  IconHeart,
+  IconArrowUpRight,
+  IconChevronDown,
+}from '@tabler/icon-react'
+
+
+function StatCard({
+  title,
+  value,
+  subText,
+  subColour,
+  icon,
+}: {
+  title: string
+  value: string
+  subText: string
+  subColour: string
+  icon: React.ReactNode
+}){
+  return(
+    <div className="bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden">
+      
+      <div className="bg-[#003366] px-4 py-2 flex items-center justify-between">
+        <p className="text-white text-sm font-bold">{title}</p>
+        <span className="text-white/70">{icon}</span>
+      </div>
+    
+      <div className="px-4 py-3">
+        <p className="text-2xl font-bold text-gray-800">{value}</p>
+        <p className={`text-xs mt-1 ${subColor}`}>{subText}</p>
+      </div>
+    </div>
+  )
+}
+
+
 export default function BuyerDashboard() {
   return (
     <div>
