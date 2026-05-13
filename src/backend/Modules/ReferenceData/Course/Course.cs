d17099882b5b9f5e5f3b4d85ce35c765ebd99e59
Zelamene <u24742083@tuks.co.za>
2026-05-13 06:23:32 +0200

    fIx type mismatch

--- src/backend/Modules/ReferenceData/Course/Course.cs
+++ src/backend/Modules/ReferenceData/Course/Course.cs
@@ -5,7 +5,7 @@ public class Course
     public int CourseId { get; set; }
     public int UniversityId { get; set; }
     public string CourseCode { get; set; }
-    public bool CourseName { get; set; } = true;
+    public string CourseName { get; set; }
 
     public string Faculty { get; set; }
 
