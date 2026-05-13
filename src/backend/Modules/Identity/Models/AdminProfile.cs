ec5d554108e5e02dd576335fb078acd93b96b62c
Zelamene <u24742083@tuks.co.za>
2026-05-13 05:40:57 +0200

    Create AdminProfile Model

---
+++ src/backend/Modules/Identity/Models/AdminProfile.cs
@@ -0,0 +1,8 @@
+namespace Modules.Identity.Models;
+
+public class AdminProfile
+{
+    public Guid AdminId { get; set; }
+    public Guid UserId { get; set; }
+    public int UniversityId { get; set; }
+}
