203645d04616f8772867d9e0c98e97237287d25a
Zelamene <u24742083@tuks.co.za>
2026-05-13 05:26:47 +0200

    Create User Model

---
+++ src/backend/Modules/Identity/Models/User.cs
@@ -0,0 +1,15 @@
+namespace Modules.Identity.Models;
+
+public class User
+{
+    public Guid UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string PasswordHash { get; set; }
+    public string Role { get; set; } // admin or student
+    public Datetime CreatedAt { get; set; }
+    public Datetime UpdatedAt { get; set; }
+
+}
