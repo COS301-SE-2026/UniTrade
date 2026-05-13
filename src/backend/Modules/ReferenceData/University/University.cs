65fdd975dc744d4c2f77f90669ecdc89819b9d39
Zelamene <u24742083@tuks.co.za>
2026-05-13 06:03:21 +0200

    Create shared reference modules

---
+++ src/backend/Modules/ReferenceData/University/University.cs
@@ -0,0 +1,11 @@
+namespace Modules.ReferenceData.University;
+
+public class University
+{
+    public int UniversityId { get; set; }
+    public string Name { get; set; }
+    public string EmailDomain { get; set; }
+    public bool IsActive { get; set; } = true;
+
+
+}
