34dc7eaa8f500e813539638b4a4a91f969b514bd
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-14 19:12:41 +0200

    updated program config

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -1,4 +1,8 @@
-using API.Infrastructure.Persistence
+using API.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Modules.Identity; 
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,27 @@ builder.Services.AddDbContext<AppDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+//service layer for login endpoint
+builder.Services.AddScoped<IIdentityService,IdentityService>();
+
+//authentication setup(jwt)
+var jwtSecret=builder.Configuration["JWT_SECRET"];
+var key= Encoding.ASCII.GetBytes(jwtSecret);
+
+builder.Services.AddAuthentication(options=>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey=true,
+        IssuerSigningKey= new SymmetricSecurityKey(key),
+        ValidateLifetime=true
+    }
+});
+
 var app = builder.Build();
 
 // Configure middleware pipeline
@@ -22,6 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
