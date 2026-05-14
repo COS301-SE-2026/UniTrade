a5399625b7dcbb3b045639f725fdc01004fa609d
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-14 11:54:43 +0200

    connected db via program cs

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -1,9 +1,16 @@
+using API.Infrastructure.Persistence
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//creates a dbcpntext when you need it. reads conn from appsettings
+builder.Services.AddDbContext<AppDbContext>(options=>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
