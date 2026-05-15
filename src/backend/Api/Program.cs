c09c7f4cfcc3b0435e022a4b0cb8b35104590cc8
Zelamene <u24742083@tuks.co.za>
2026-05-15 04:02:09 +0200

    Configure persistence layer

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -1,4 +1,25 @@
+using dotenv.net;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+
+DotEnv.Load(options: new DotEnvOptions(
+    envFilePaths: new[] { Path.Combine(Directory.GetCurrentDirectory(), "../.env") }
+));
+
 var builder = WebApplication.CreateBuilder(args);
+
+// configs 
+builder.Configuration.AddEnvironmentVariables();
+
+// db context 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:Connection"]
+    );
+});
+
 
 // Add services to the container
 builder.Services.AddControllers();
@@ -7,6 +28,23 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// -- Test DB Connection
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    try
+    {
+        var canConnect = db.Database.CanConnect();
+        Console.WriteLine(canConnect ? "Database connection successful" : "MEHHHH");
+
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine($"DB ERROR: {ex.Message}");
+    }
+}
 // Configure middleware pipeline
 if (app.Environment.IsDevelopment())
 {
--- src/backend/Modules/Identity/Models/AdminProfile.cs
+++ src/backend/Modules/Identity/Models/AdminProfile.cs
@@ -5,4 +5,7 @@ public class AdminProfile
     public Guid AdminId { get; set; }
     public Guid UserId { get; set; }
     public int UniversityId { get; set; }
+
+    public User User { get; set; } = null!;
+
 }
