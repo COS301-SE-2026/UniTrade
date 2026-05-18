128ff3f8c605ac482e0d86a0017c3aa788f7e4ad
diditlaka <diditlaka51@gmail.com>
2026-05-18 13:44:12 +0200

    added the category card for the browse listing page

--- src/web/src/pages/buyer/BrowseAllListing.tsx
+++ src/web/src/pages/buyer/BrowseAllListing.tsx
@@ -1,6 +1,29 @@
 import {useNavigate} from 'react-router-dom'
-import {useAuthoStore} from '../../store/useAuthStore'
+import {useAuthStore} from '../../store/useAuthStore'
 import calculasTextbook from '../../assests/calculas-textbook.jpg'
 import pencil from '../../mechanical-pencil.jpg'
 import laptop from '../../hp-laptop.jpg'
+import {useState} from 'react'
+
+function CategoryCard({
+    title,
+    active,
+    onClick,
+}:{
+    title:string
+    active: boolean
+    onClick: () => void
+}) {
+    return(
+        <button
+        onClick={onClick}
+        className= {`px-5 py-2 rounded-full border text-sm font-medium transition-colors ${active ? 'bg-[#003366] text-white border-[#003366]' : 'bg-white text-gray-700 border-gray-300 hover:border-[#00336]'}`}
+        >
+        </button>
+    )
+}
+
+
+
+
 
