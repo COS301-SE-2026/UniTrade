47a385ba7890d38dadb47126b745099abddd86f1
diditlaka <diditlaka51@gmail.com>
2026-05-18 10:48:31 +0200

    : created the page for browse all listings and the images as well

---
+++ src/web/src/pages/buyer/BrowseAllListing.tsx
@@ -0,0 +1,6 @@
+import {useNavigate} from 'react-router-dom'
+import {useAuthoStore} from '../../store/useAuthStore'
+import calculasTextbook from '../../assests/calculas-textbook.jpg'
+import pencil from '../../mechanical-pencil.jpg'
+import laptop from '../../hp-laptop.jpg'
+
