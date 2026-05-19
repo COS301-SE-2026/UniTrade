301f6f3e35a932701388ae60fe14711661141338
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-20 00:51:59 +0200

    fixed return type

--- src/backend/Modules/Listings/ListingsService.cs
+++ src/backend/Modules/Listings/ListingsService.cs
@@ -39,7 +39,7 @@ namespace Modules.Listings
             var listingLookUp=await _dbContext.Listing.FindAsync(id);
             if(listingLookUp==null)
             { 
-                return null;
+                return false;
             }
 
             listingLookUp.Title=listings.Title;
@@ -50,7 +50,7 @@ namespace Modules.Listings
 
             await _dbContext.SaveChangesAsync();
 
-            return listingLookUp;
+            return true;
         }
 
 
