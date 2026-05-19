1b35bdccb2aaada9068354a19387136a1866f460
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-19 21:59:31 +0200

    added skeleton code for create listings endp

---
+++ src/backend/Modules/Listings/ListingsService.cs
@@ -0,0 +1,12 @@
+using Modules.Listings.Models;
+using Modules.Listings.Models.DTO;
+
+namespace Modules.Listings
+{
+    public ListingsService: base IListingsService
+    {
+        public 
+    }
+}
+
+
