311d54c86aba291ea08d8d53ec52f9620edc39c9
Zelamene <u24742083@tuks.co.za>
2026-05-20 04:25:14 +0200

    remove unnecessary packages

--- src/backend/Modules/Identity/Models/adminProf.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Modules.Identity.Models
-{
-    public class AdminProf
-    {
-        public Guid AdminId { get; set; }
-        public Guid UserId { get; set; }
-        public int University_ID { get; set; }
-    }
-}
