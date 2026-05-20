881cfbe059e031883e08feb2060ef24be3307f88
Tafadzwa2608 <tafadzwamusiiwa08@gmail.com>
2026-05-21 01:17:49 +0200

    Updated authService.ts and authErrors.ts

--- src/web/src/utils/authErrors.ts
+++ src/web/src/utils/authErrors.ts
@@ -3,7 +3,7 @@ export function getAuthErrorMessage(code: string): string {
     invalid_email:          'Please enter a valid student email address.',
     invalid_domain:         'Email domain is not a recognised SA university.',
     email_taken:            'An account with this email already exists.',
-    weak_password:          'Password is too weak. Use at least 8 characters.',
+    weak_password:          'Password is too weak. Use at least 8 characters with at laest one capital letter, at least one special character and at least one number.',
     invalid_year_of_study:  'Please enter a valid year of study.',
     otp_already_sent:       'An OTP was already sent. Please wait before requesting another.',
     invalid_otp:            'The OTP you entered is incorrect.',
