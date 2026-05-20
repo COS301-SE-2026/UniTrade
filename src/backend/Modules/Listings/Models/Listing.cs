756cbd305c802d6a10d9efd562ab2b722ae01f5e
Zelamene <u24742083@tuks.co.za>
2026-05-20 08:47:42 +0200

    add listing entities

---
+++ src/backend/Modules/Listings/Models/Listing.cs
@@ -0,0 +1,40 @@
+using System.Reflection.Metadata;
+using Modules.Identity.Modules;
+
+namespace Modules.Listings.Modules;
+
+public class Listings
+{
+    public Guid ListingId { get; set; }
+    public Guid SellerId { get; set; }
+
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public decimal Price { get; set; }
+    public string Condition { get; set; } = "good";
+    public string ListingType { get; set; } = "other";
+
+    public int? CourseId { get; set; }
+    public string? Isbn { get; set; }
+    public string? Author { get; set; }
+    public string? Edition { get; set; }
+
+    public string ListingStatus { get; set; }
+
+    // not in MVP
+    public decimal? AiRiskScore { get; set; }
+    public string? AiRiskLevel { get; set; }
+    public int? VisibilityScore { get; set; }
+    public bool? isBundle { get; set; }
+    public string? RejectionReason { get; set; }
+    public int? ViewCount { get; set; }
+    //===========
+
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public UserStringHandle? Seller { get; set; }
+    public ICollection<ListingImage> Images { get; set; } = new List<ListingImage>();
+
+
+}
