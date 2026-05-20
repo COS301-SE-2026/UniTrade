10321133ad2db6c1b4bd329a4fa6c01ed134366d
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-20 12:00:05 +0200

    updated listingsrepo interface

---
+++ src/backend/Modules/Listings/Repositories/IListingsRepository.cs
@@ -0,0 +1,12 @@
+using Modules.Listings;
+using Modules.Listings.Models;
+
+namespace Modules.Listings.Repositories;
+
+public interface ListingsRepository
+{
+    Task AddAsync(ListingsModel listings);
+    Task UpdateAsync(ListingsModel listings,Guid id);
+    Task<User?> DeleteByIdAsync(Guid id);
+
+}
