8a67a4b36b17e3f3e4f2497562984d6f43268b6c
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-20 18:35:28 +0200

    fixed some syntax

--- src/backend/Modules/Listings/IListingsService.cs
+++ src/backend/Modules/Listings/IListingsService.cs
@@ -3,11 +3,11 @@ namespace Modules.Listings.Models;
 
 public interface IListingsService
 {
-    {
+   
         Task<Listings> CreateListings(ListingsDto listings);//retruning a listings-> so when
         //users can see WHEN it was created and so on.P.s could be void, but retruning is stardard procedure.
         Task<bool> UpdateListings(ListingsDto listings, int id);
         Task<bool> DeleteListings(int id);
 
-    }
+
 }
