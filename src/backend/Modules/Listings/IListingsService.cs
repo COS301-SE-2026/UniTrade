997add502cb371b4962604d023607e8bbe4c15b8
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-20 11:58:12 +0200

    updated listingsrepo interface

--- src/backend/Modules/Listings/IListingsService.cs
+++ src/backend/Modules/Listings/IListingsService.cs
@@ -7,7 +7,7 @@ using Modules.Listings.Models.DTO;
         Task<Listings> CreateListings(CreateListingsDto listings);//retruning a listings-> so when
         //users can see WHEN it was created and so on.P.s could be void, but retruning is stardard procedure.
         Task<bool> UpdateListings(CreateListingsDto listings, int id);
-        Task<bool> DeleteLsitings( int id);
+        Task<bool> DeleteListings(int id);
 
     }
 }
