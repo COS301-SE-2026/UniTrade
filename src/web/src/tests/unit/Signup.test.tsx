5946fb679592918c7ae36aa69c5df540de070836
langa-vk <langavaks@gmail.com>
2026-05-20 14:03:25 +0200

    signup test#2

--- src/web/src/tests/unit/Signup.test.tsx
+++ src/web/src/tests/unit/Signup.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, screen,fireEvent, waitFor} from '@testing-library/react'
 import {describe,test,expect, beforeEach, vi,it} from 'vitest';
-import Login from '../../pages/auth/Signup';
+import Signup from '../../pages/auth/Signup';
 
 //mock useNavigate
 const mockPost = vi.fn();
@@ -23,7 +23,7 @@ const formFilled = (overrides: Record<string, string> = {}) => {
         lastName: 'Vakalisa',
         email: 'langavaks@gmail.com',
         university: 'UP',
-        degree: 'Computer Science',
+        degreeProgram: 'Computer Science',
         yearOfStudy: '3',
         password: 'Password@100',
     };
@@ -36,7 +36,7 @@ fireEvent.change(screen.getByPlaceholderText('Last Name'), { target: { value: va
 fireEvent.change(screen.getByPlaceholderText('Email'), { target: { value: values.email ,name: 'email' } });
 fireEvent.change(screen.getByDisplayValue('Select University'), {target:{value: values.university, name: 'university'}});
 fireEvent.change(screen.getByPlaceholderText('Degree Program'), { target: { value: values.degreeProgram ,name: 'degreeProgram' } });
-fireEvent.change(screen.getByPlaceholderText('YearOfStudy'), { target: { value: values.yearOfStudy ,name: 'yearOfStudy' } });
+fireEvent.change(screen.getByPlaceholderText('Year of Study'), { target: { value: values.yearOfStudy ,name: 'yearOfStudy' } });
 
 fireEvent.change(screen.getByPlaceholderText('Password'), { target: { value: values.password ,name: 'password' } });
 };
@@ -55,11 +55,11 @@ beforeEach(() => {
 
 
 test( 'should render form fields and Signup button', () => {
-    render(<Login />);
+    render(<Signup />);
 
     expect(screen.getByText('Get Started')).toBeInTheDocument();
    // expect(screen.getByText('Enter your credentials to access your account')).toBeInTheDocument();
-        expect(screen.getByPlaceholderText('First Name')).toBeInTheDocument();
+     expect(screen.getByPlaceholderText('First Name')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Last Name')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
     expect(screen.getByDisplayValue('Select University')).toBeInTheDocument();
@@ -68,6 +68,23 @@ test( 'should render form fields and Signup button', () => {
     expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /signup/i })).toBeInTheDocument();
 });
+
+
+test('should update fields when typing', () => {
+    render(<Signup />);
+    formFilled();
+
+   expect(screen.getByPlaceholderText('First Name')).toHaveValue('Langa');
+    expect(screen.getByPlaceholderText('Last Name')).toHaveValue('Vakalisa');
+    expect(screen.getByPlaceholderText('Email')).toHaveValue('langavaks@gmail.com');
+    expect(screen.getByDisplayValue('University of Pretoria')).toBeInTheDocument
+    expect(screen.getByPlaceholderText('Degree Program')).toHaveValue('Computer Science');
+    expect(screen.getByPlaceholderText('Year of Study')).toHaveValue('3');
+    expect(screen.getByPlaceholderText('Password')).toHaveValue('Password@100');
+
+
+});
+
 
 
 });
