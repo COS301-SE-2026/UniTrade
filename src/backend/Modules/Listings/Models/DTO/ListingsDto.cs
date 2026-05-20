6723b98b82db675d067076a32e94bc1ee568fb8f
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-20 17:51:53 +0200

    added new dto

---
+++ src/backend/Modules/Listings/Models/DTO/ListingsDto.cs
@@ -0,0 +1,17 @@
+namespace Modules.Listings.Models.DTO
+{
+    public class ListingsDto
+    {
+        //remeber dto is the client reqq!!!
+        [Required] //fail-fast property(see if there is more stricter ones to add later)
+        public string Title{get;set;}
+
+        public string Description{get;set;}
+
+        [Required]
+        public NUMERIC Price{get;set;}
+
+        [Required]
+        public string Condition{get;set;}
+    }
+}
