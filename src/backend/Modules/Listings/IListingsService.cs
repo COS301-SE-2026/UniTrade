62fd48cd8cbd1f9bd6ef417fe6fa69e693cefdd1
Sabira-k-commits <u23591481@tuks.co.za>
2026-05-20 17:48:09 +0200

    changed dto name

--- src/backend/Modules/Listings/IListingsService.cs
+++ src/backend/Modules/Listings/IListingsService.cs
@@ -4,9 +4,9 @@ using Modules.Listings.Models.DTO;
 {
     public interface IListingsService
     {
-        Task<Listings> CreateListings(CreateListingsDto listings);//retruning a listings-> so when
+        Task<Listings> CreateListings(ListingsDto listings);//retruning a listings-> so when
         //users can see WHEN it was created and so on.P.s could be void, but retruning is stardard procedure.
-        Task<bool> UpdateListings(CreateListingsDto listings, int id);
+        Task<bool> UpdateListings(ListingsDto listings, int id);
         Task<bool> DeleteListings(int id);
 
     }
