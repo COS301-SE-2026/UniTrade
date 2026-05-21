683f3a709c727106eb81ed5d264598a0de83c811
Zelamene <u24742083@tuks.co.za>
2026-05-21 20:29:21 +0200

    Add image storage(Azure)

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -17,6 +17,8 @@ using Microsoft.AspNetCore.Http.Json;
 using Modules.Listings;
 using Modules.Listings.Repositories;
 using Infrastructure.Persistence.Repositories.Listings;
+using Infrastructure.Storage;
+using Modules.SharedKernel;
 
 
 DotEnv.Load(options: new DotEnvOptions(
@@ -86,7 +88,7 @@ builder.Services.AddScoped<IVerificationService, VerificationService>();
 builder.Services.AddHttpClient<INotificationsService, ResendEmailService>();
 builder.Services.AddScoped<IListingService, ListingService>();
 builder.Services.AddScoped<IListingRepository, ListingRepository>();
-
+builder.Services.AddSingleton<IBlobStorageService, BlobStorageService>();
 var jwtSecret = builder.Configuration["Jwt:Secret"]
     ?? throw new InvalidOperationException("JWT_SECRET is not configured");
 var key = Encoding.UTF8.GetBytes(jwtSecret);
