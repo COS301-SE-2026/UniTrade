9205e81655be250733109862de17d5565e9b9f35
Tafadzwa2608 <tafadzwamusiiwa08@gmail.com>
2026-05-21 20:24:19 +0200

    Changes

--- src/backend/Modules/Listings/IListingsService.cs
+++ src/backend/Modules/Listings/IListingsService.cs
@@ -7,9 +7,8 @@ namespace Modules.Listings;
 
 public interface IListingService
 {
-    Task<ListingSummaryDto> CreateListings(ListingSummaryDto listings);//retruning a listings-> so when
-                                                             //users can see WHEN it was created and so on.P.s could be void, but retruning is stardard procedure.
-    Task<bool> UpdateListings(ListingSummaryDto listings, Guid id);
+    Task<ListingSummaryDto> CreateListings(ListingSummaryDto listings);
+    Task<bool> UpdateListings(UpdateListingDto listings, Guid id);
     Task<bool> DeleteListings(Guid id);
     Task<ListingSummaryDto?> GetByIdAsync(Guid listingId);
     Task<PagedResult<ListingSummaryDto>> ListAsync(ListFilterDto filter);
