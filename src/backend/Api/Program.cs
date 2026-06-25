110f4d506d6234450ee5728ad1aa3d19ab8a297c
Zelamene <u24742083@tuks.co.za>
2026-06-26 01:48:47 +0200

    Implement Rate Limiting on OTP Verification Endpoint

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -30,6 +30,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
 
+//rate limiters
+
 builder.Services.AddRateLimiter(options =>
 {
     options.AddPolicy(
@@ -55,6 +57,20 @@ builder.Services.AddRateLimiter(options =>
                 {
                     PermitLimit = 10,
                     Window = TimeSpan.FromMinutes(15),
+                    QueueLimit = 0,
+                }
+            )
+    );
+
+    options.AddPolicy(
+        "verify-otp",
+        httpContext =>
+            RateLimitPartition.GetFixedWindowLimiter(
+                httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+                _ => new FixedWindowRateLimiterOptions
+                {
+                    PermitLimit = 5,
+                    Window = TimeSpan.FromMinutes(1),
                     QueueLimit = 0,
                 }
             )
