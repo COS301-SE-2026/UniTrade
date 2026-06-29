f395f0f7eb0bfde1f53a93bbe36dc99c7669da71
Sabira-k-commits <u23591481@tuks.co.za>
2026-06-29 18:24:04 +0200

    fixed another err

--- src/backend/Modules/ReferenceData/University/University.cs
+++ src/backend/Modules/ReferenceData/University/University.cs
@@ -2,7 +2,7 @@ namespace Modules.ReferenceData.University;
 
 public class University
 {
-    public int UniversityId { get; set; }
+    public Guid UniversityId { get; set; }
     public string Name { get; set; } = null!;
     public string EmailDomain { get; set; } = null!;
     public bool IsActive { get; set; } = true;
