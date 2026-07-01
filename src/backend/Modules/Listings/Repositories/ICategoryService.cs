66119aa6d0b65f2e4104f109f703d24e8dc765e3
Sabira-k-commits <u23591481@tuks.co.za>
2026-07-02 00:07:30 +0200

    removed unneccessary file

--- src/backend/Modules/Listings/Repositories/ICategoryService.cs
+++
@@ -1,8 +0,0 @@
-using Modules.Listings.Repositories;
-
-namespace Modules.Listings.Repositories;
-
-public interface ICategoryService
-{
-    Task<ListingCategories?> ResolveByNameAsync(string categoryName, CancellationToken ct=default);
-}
