f07d2401d2bbab7b01e2ee4aa0a28ad054f0c9b9
Sabira-k-commits <u23591481@tuks.co.za>
2026-07-01 20:07:16 +0200

    updated mappings of models

--- src/backend/Modules/Listings/Models/Listing.cs
+++ src/backend/Modules/Listings/Models/Listing.cs
@@ -3,54 +3,6 @@ using System.Reflection.Metadata;
 
 namespace Modules.Listings.Models;
 
-/*
-CREATE TABLE Listings (
-    listing_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    seller_id UUID NOT NULL,
-    category_id INT NOT NULL,
-    title VARCHAR(150) NOT NULL,
-    description TEXT NOT NULL,
-    price NUMERIC(10, 2) NOT NULL CONSTRAINT chk_listing_price CHECK (price > 0),
-    condition VARCHAR(5) NOT NULL CONSTRAINT chk_listing_condition CHECK(
-        condition IN ('new', 'good', 'fair', 'poor')
-    ),
-    course_id INT NULL,
-    metadata JSONB NULL,
-    listing_status VARCHAR(20) NOT NULL CONSTRAINT chk_listing_status CHECK (
-        listing_status IN (
-            'draft',
-            'pending',
-            'live',
-            'low_visibility',
-            'rejected',
-            'sold',
-            'removed'
-        )
-    ),
-    -- AI Mod (nullable; not implemented in MVP)
-    ai_risk_score NUMERIC(5, 2) NULL,
-    ai_risk_level VARCHAR(10) NULL CONSTRAINT chk_listing_risk CHECK (ai_risk_level IN ('low', 'medium', 'high')),
-    visibility_score INT NULL DEFAULT 100,
-    is_bundle BOOLEAN NULL DEFAULT FALSE,
-    rejection_reason TEXT NULL,
-    view_count INT NULL DEFAULT 0,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    CONSTRAINT fk_listings_category FOREIGN KEY (category_id) REFERENCES Listing_category(category_id),
-    CONSTRAINT fk_listings_users FOREIGN KEY (seller_id) REFERENCES Users(user_id) ON DELETE NO ACTION,
-    CONSTRAINT fk_listings_course FOREIGN KEY (course_id) REFERENCES Course(course_id) ON DELETE NO ACTION
-);
-
--- 7.1 Listing category
-CREATE TABLE Listing_category(
-    category_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    root_category_id INT NULL REFERENCES Listing_category(category_id),
-    is_active BOOLEAN NOT NULL DEFAULT TRUE
-);
-
-*/
-
 public class Listing
 {
     public Guid ListingId { get; set; }
