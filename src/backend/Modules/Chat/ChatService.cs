641aea16eae083b2fabd74655961e43d6e7c8eb9
Sabira-k-commits <u23591481@tuks.co.za>
2026-07-10 09:39:31 +0200

    added func

--- src/backend/Modules/Chat/ChatService.cs
+++ src/backend/Modules/Chat/ChatService.cs
@@ -1,13 +1,48 @@
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using Modules.Chat.Models;
+using Modules.Chat.Models.Dto;
+using Modules.Reservations;
+
 namespace Modules.Chat;
 
 public class ChatService: IChatService
 {
-    private readonly IReservationService _reservationService;
+    private readonly IReservationService _reservationService;//using Isuserpat of reseravtion func
     private readonly AppDbContext _context; ///swap out for repo
 
     public ChatService(AppDbContext context, IReservationService reservationSerice)
     {
         _context=context;
         _reservationService=reservationSerice;
+    }
+
+    public async Task<ChatMessageDto> SendAsync(Guid reservationId, Guid senderId,string content, CancellationToken ct=default)
+    {
+        if(string.IsNullOrWhiteSpace(content))
+        {
+            throw ne ArgumentException("Message content cannot be empty");
+        }
+
+        var isAuthorised= await _reservationService.IsUserReservedAsync(senderId,reservationId);
+
+        if(!isAuthorised)
+        {
+            throw new UnauthorisedAccessException("You are not a participant of this reservation");
+        }
+
+        var result=new ChatMessageDto{
+            ReservationId=reservationId,
+            SenderId=senderId,
+            MessageType="text",
+            Content=content,
+            SentAt=DateTime.UtcNow
+        };
+
+        _context.ChatMessages.Add(result);
+
+        await _context.SaveChanges.Async(ct);
+
+        return ToDoto(result);
     }
 }
