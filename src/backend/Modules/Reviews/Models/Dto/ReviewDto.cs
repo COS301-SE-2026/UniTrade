1df92f867a96abef68e43d493f47d9933cc14d63
Zelamene <u24742083@tuks.co.za>
2026-07-18 00:58:40 +0200

    implement review repository

---
+++ src/backend/Modules/Reviews/Models/Dto/ReviewDto.cs
@@ -0,0 +1,21 @@
+namespace Modules.Reviews.Models.Dto;
+
+public record CreateReviewRequest(Guid reservationId, int Rating, string? Comment);
+
+public record ReviewDto(
+    int ReviewId,
+    Guid TransactionId,
+    Guid ReviewerId,
+    Guid RevieweeId,
+    string ReviewType,
+    int Rating,
+    string? Comment,
+    DateTime CreatedAt
+);
+
+public record UserReviewDto(
+    Guid UserId,
+    decimal SellerScore, // to be used as the avg of buyer_to_seller reviews
+    decimal BuyerScore,
+    IReadOnlyList<ReviewDto> Reviews
+);
