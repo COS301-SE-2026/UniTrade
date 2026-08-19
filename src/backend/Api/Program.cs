f840a62a8287ca4551308ea51a333e316adc294a
Zelamene <u24742083@tuks.co.za>
2026-08-19 20:28:56 +0200

    fix formatting

--- src/backend/Api/Program.cs
+++ src/backend/Api/Program.cs
@@ -55,7 +55,7 @@ DotEnv.Load(
         envFilePaths: new[] { Path.Combine(Directory.GetCurrentDirectory(), "../.env") }
     )
 );
-ThreadPool.SetMinThreads(workerThreads:100, completionPortThreads: 100);
+ThreadPool.SetMinThreads(workerThreads: 100, completionPortThreads: 100);
 
 var builder = WebApplication.CreateBuilder(args);
 
