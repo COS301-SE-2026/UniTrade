9ef8f21ce948cb155dbedf191aab3f1aac76bb12
Zelamene <u24742083@tuks.co.za>
2026-09-01 23:29:10 +0200

    fix failing tests

--- src/backend/Tests/Unit/Modules/ListingServiceTests.cs
+++ src/backend/Tests/Unit/Modules/ListingServiceTests.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
 using Modules.Identity.Models;
 using Modules.Identity.Verification;
 using Modules.Listings;
@@ -24,6 +25,7 @@ public class ListingServiceTests
     private readonly Mock<IListingImageRepository> _imageRepo;
     private readonly Mock<ISellerVerificationQuery> _verificationMock;
     private readonly Mock<IListingPublishedListener> _listingPublishedListenerMock;
+    private readonly Mock<ILogger<ListingService>> _loggerMock;
 
     private readonly ListingService _sut;
 
@@ -33,11 +35,13 @@ public class ListingServiceTests
         _imageRepo = new Mock<IListingImageRepository>();
         _verificationMock = new Mock<ISellerVerificationQuery>();
         _listingPublishedListenerMock = new Mock<IListingPublishedListener>();
+        _loggerMock = new Mock<ILogger<ListingService>>();
         _sut = new ListingService(
             _repo.Object,
             _imageRepo.Object,
             _verificationMock.Object,
-            _listingPublishedListenerMock.Object
+            _listingPublishedListenerMock.Object,
+            _loggerMock.Object
         );
     }
 
