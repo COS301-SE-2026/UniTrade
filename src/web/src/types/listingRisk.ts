7d00e9e35048989a91bf16ffba0d0f7ea031d9ab
diditlaka <diditlaka51@gmail.com>
2026-09-19 21:26:02 +0200

    additional changes

--- src/web/src/types/listingRisk.ts
+++ src/web/src/types/listingRisk.ts
@@ -2,3 +2,10 @@ export type RiskLevel = "low" | "medium" | "high"
 
 export type FlageedListingStatus = "under_review"
 
+export type KnownRiskReason = 
+| "price_anomaly"
+| "duplicate_image"
+| "weak_description"
+| "seller_history"
+| "image_mismatch";
+
