bfa9d7b144d6a7ff4357f9cf9902ac08f2436e06
diditlaka <diditlaka51@gmail.com>
2026-09-26 10:31:57 +0200

    new request

---
+++ src/backend/Modules/Identity/Models/PasswordResetRequest.cs
@@ -0,0 +1,15 @@
+namespace Modules.Identity.Models;
+
+public class PasswordResetRequest
+{
+    public Guid PasswordResetRequestId { get; set;}
+    public Guid UserId {get; set;}
+
+    public string OtpCodeHash { get; set;} = null!;
+    public DateTime OtpSentAt {get; set;} = DateTime.UtcNow;
+    public DateTimes OtpExpiresAt {get; set;}
+    public DateTime? OtpVerifiedAt {get; set;}
+
+    public int AttemptNumber {get; set;} = 0;
+    public bool IsCurrent {get; set;} = true;
+}
